106137725bba022113164a4494a9bedb8e48057d
Athang69 <athangkali21@gmail.com>
2025-05-29 09:11:05 +0530

    Next Step Added the notification counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,23 @@
 import { PostComponent } from "./Post";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 function App(){
-  const [posts,setPosts]=useState([]);
-
-  const postComponents= posts.map(post=> <PostComponent
-    name={post.name}
-    subtitle={post.subtitle}
-    time={post.time}
-    image={post.image}
-    description={post.description}
-  />)
-
-  function addPosts(){
-    setPosts([...posts,{
-      name:"Athang",
-      subtitle:"How to learn react in 7 days",
-      time:"5m ago",
-      image:"https://appx-wsb-gcp.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg",
-      description:"This will guide you on how to become a 100x dev in no time"
-    },{name:"Athang",
-      subtitle:"How to learn react in 7 days",
-      time:"5m ago",
-      image:"https://appx-wsb-gcp.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg",
-      description:"This will guide you on how to become a 100x dev in no time"}])
+  const [count,setCount]=useState(1);
+  function incrNoti(){
+    setCount(function(currentVal){
+      return currentVal+1;
+    })
   }
-  return(
-    <div style={{background:"#dfe6e9", height:"100vh"}}>
-      <button onClick={addPosts}>Add Posts</button>
-      <div style={{display:"flex", justifyContent:"center"}}>
-        <div>
-          <div style={{marginBottom:30}}>
-            {postComponents}
-          </div>
-        </div>
-      </div>
-    </div>
-  )
 
+useEffect(function(){
+  setInterval(incrNoti,1000)
+},[])
+
+
+ return <div>
+    <div style={{background:"red",height:20, width:20, borderRadius:"50%", marginLeft:15, display:"flex", justifyContent:"center"}}>
+      {count}
+    </div>
+    <img src="https://www.svgrepo.com/show/491798/bell-notification-ui.svg" style={{width:35, height:35, background:"white"}} />
+  </div>
 }
 export default App;
